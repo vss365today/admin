490818231d24a402068b60c666e5ead702ab51cd
Caleb Ely <le717@users.noreply.github.com>
2021-01-23 16:59:11 -0500

    More db actions

--- src/core/database/core.py
+++ src/core/database/core.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Iterator
 
 
-__all__ = ["connect_to_db", "get_sql"]
+__all__ = ["connect_to_db", "convert_int_to_bool", "convert_bool_to_int", "get_sql"]
 
 
 @contextmanager
@@ -19,6 +19,22 @@ def connect_to_db() -> Iterator[sqlite3.Cursor]:
         db.close()
 
 
-def get_sql(filename: str) -> str:
+def convert_int_to_bool(data: dict) -> dict:
+    """Convert all integer values into a Boolean."""
+    for k, v in data.items():
+        if type(v) == int:
+            data[k] = bool(v)
+    return data
+
+
+def convert_bool_to_int(data: dict) -> dict:
+    """Convert all Boolean values into a integer."""
+    for k, v in data.items():
+        if type(v) == bool:
+            data[k] = int(v)
+    return data
+
+
+def get_sql(script_name: str) -> str:
     """Get the contents of a SQL script."""
-    return (Path() / "db" / f"{filename}.sql").read_text()
+    return (Path() / "db" / f"{script_name}.sql").read_text()
