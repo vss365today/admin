f30fe69b518268513828e53837832b7c6255a32c
Caleb Ely <le717@users.noreply.github.com>
2021-01-23 16:33:16 -0500

    Make login work for real

--- src/blueprint/root.py
+++ src/blueprint/root.py
@@ -1,10 +1,31 @@
-from flask import render_template
+from flask import flash, redirect, render_template, url_for
 
 from src.blueprint import bp_root as root
+from src.core.database import users
 from src.core.forms import FormUserLogin
 
 
 @root.route("/")
-def index():
+def index() -> str:
+    """Site login page."""
     render_opts = {"form": FormUserLogin()}
     return render_template("root.html", **render_opts)
+
+
+@root.route("login", methods=["POST"])
+def login():
+    """Login a user."""
+    # Confirm we have form data
+    form = FormUserLogin()
+    if form.validate_on_submit():
+
+        # Validate the username/password combo
+        valid_login = users.login(form.data["username"], form.data["password"])
+
+        # That didn't work
+        if not valid_login:
+            flash("That is not a valid login.", "error")
+            return redirect(url_for("root.index"))
+
+    # TODO Redirect to the landing page
+    return "Hello, world"
