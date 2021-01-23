93d7e4cb228132f147d9e4cb6fe372440325dd08
Caleb Ely <le717@users.noreply.github.com>
2021-01-23 15:54:00 -0500

    Add initial users db methods

--- src/core/database/users.py
+++ src/core/database/users.py
@@ -1,4 +1,23 @@
+from sqlite3 import Row
+from typing import Optional
+
 from src.core.database.core import connect_to_db, get_sql
 
 
-__all__ = []
+__all__ = ["get_info", "login"]
+
+
+def get_info(username: str) -> Optional[Row]:
+    """Get the user's information."""
+    sql = get_sql("user-fetch-info")
+    with connect_to_db() as db:
+        db.execute(sql, {"username": username})
+        return db.fetchone()
+
+
+def login(username: str, password: str) -> Optional[Row]:
+    """Atempt to login a user."""
+    sql = get_sql("user-login")
+    with connect_to_db() as db:
+        db.execute(sql, {"username": username, "password": password})
+        return db.fetchone()
