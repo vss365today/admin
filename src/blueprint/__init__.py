232ba3f25371e2b673d79f14db0d4feff5c7fc16
Caleb Ely <le717@users.noreply.github.com>
2021-01-26 13:29:05 -0500

    Add authorization to blueprints

--- src/blueprint/__init__.py
+++ src/blueprint/__init__.py
@@ -2,12 +2,11 @@ from typing import Callable, Optional
 
 from flask import Blueprint
 
+from src.core.auth_helpers import authorize_blueprint
+
 
 def _factory(
-    partial_module_string: str,
-    url_prefix: str,
-    protected: bool = False,
-    auth_function: Optional[Callable] = None,
+    partial_module_string: str, url_prefix: str, protected: bool = False
 ) -> Blueprint:
     # Create the blueprint
     blueprint = Blueprint(
@@ -16,22 +15,16 @@ def _factory(
         url_prefix=url_prefix,
     )
 
-    # This endpoint is not to be publicly used
+    # This endpoint is not to be publicly used,
+    # protect the endpoint with an authorization routine
     if protected:
-        # Protected endpoints must have an authorization method
-        if auth_function is None:
-            raise NotImplementedError(
-                "An authorization method must be given for protected endpoints!"  # noqa
-            )
-
-        # Protect the endpoint with an authorization routine
-        blueprint.before_request(auth_function)
+        blueprint.before_request(authorize_blueprint)
     return blueprint
 
 
 bp_root = _factory("root", "/")
-bp_config = _factory("config", "/config")
-bp_host = _factory("host", "/host")
-bp_prompt = _factory("prompt", "/prompt")
+bp_config = _factory("config", "/config", True)
+bp_host = _factory("host", "/host", True)
+bp_prompt = _factory("prompt", "/prompt", True)
 
 all_blueprints = (bp_root, bp_config, bp_host, bp_prompt)
