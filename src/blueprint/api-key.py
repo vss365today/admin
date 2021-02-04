281367ffb76c853d311a94c84ca9c4101cdebd8f
Caleb Ely <le717@users.noreply.github.com>
2021-02-04 18:37:33 -0500

    Start building api key pae

--- src/blueprint/api-key.py
+++ src/blueprint/api-key.py
@@ -1,8 +1,10 @@
-from flask import abort
+from flask import render_template
 
 from src.blueprint import bp_api_key as api_key
+from src.core import api
 
 
 @api_key.route("/")
 def index():
-    abort(404)
+    render_opts = {"api_keys": api.get("api-key", params={"all": True})}
+    return render_template("api-key/index.html", **render_opts)
