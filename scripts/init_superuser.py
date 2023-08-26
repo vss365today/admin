df4ced36ce7bfa145855b36510a0c0127f1d16dd
Caleb Ely <le717@users.noreply.github.com>
2023-08-26 14:29:19 -0400

    Fix init_superuser to work again

--- scripts/init_superuser.py
+++ scripts/init_superuser.py
@@ -1,18 +1,20 @@
-import sqlite3
+import sys
 from pathlib import Path
 
 from passlib import pwd
 from passlib.hash import pbkdf2_sha256
 
-from src.core import api
+# We have to add the app path to the path to get the db
+APP_ROOT = Path(__file__).parent.parent
+sys.path.insert(0, APP_ROOT.as_posix())
+
+from db.dummy_db import create_app
+from src.core.api import v2
+from src.core.database.models import User, db
 
 
 def main() -> None:
     """Create the initial admin superuser."""
-    # TODO: This whole thing is broken atm
-    print("This script is broken. Fix it to use sqlalchemy and API v2.")
-    return None
-
     # Ask for the username
     username = input("Enter the desired username: ").strip()
     if not username:
@@ -26,47 +28,30 @@ def main() -> None:
     # Create an API token for this user, giving it all permissions
     request_body = {
         "desc": f"Admin superuser {username} API key",
-        "has_api_key": True,
         "has_archive": True,
-        "has_broadcast": True,
-        "has_host": True,
-        "has_prompt": True,
-        "has_settings": True,
-        "has_subscription": True,
+        "has_notifications": True,
+        "has_hosts": True,
+        "has_keys": True,
+        "has_prompts": True,
+        "has_emails": True,
     }
-    created_token = api.post("api-key", user_token=False, json=request_body)["token"]
-
-    # Connect to the database
-    db = sqlite3.connect((Path() / "db" / "database.db"))
-    cursor = db.cursor()
-
-    # Check if the schema exists already
-    sql_schema_exists = (Path() / "db" / "5-check-if-schema-exists.sql").read_text()
-    cursor.execute(
-        sql_schema_exists,
-        {"table_name": "users"},
-    )
-
-    # The schema doesn't exist, create it
-    if cursor.fetchone() is None:
-        cursor.execute((Path() / "db" / "0-schema.sql").read_text())
+    created_token = v2.post("keys/", user_token=False, json=request_body)["token"]
 
     # Record this user and API token in the local db
-    sql_create_user = (Path() / "db" / "1-user-create.sql").read_text()
-    sql_create_token = (Path() / "db" / "4-user-update-api-token.sql").read_text()
-    cursor.execute(
-        sql_create_user,
-        {"username": username, "password": password_hashed, "is_superuser": 1},
-    )
-    cursor.execute(
-        sql_create_token,
-        {"username": username, "api_token": created_token},
-    )
-    db.commit()
-    db.close()
+    app = create_app()
+    with app.app_context():
+        u = User(
+            username=username,
+            password=password_hashed,
+            api_token=created_token,
+            is_active=True,
+            is_superuser=True,
+        )
+        db.session.add(u)
+        db.session.commit()
 
     # Report the creation results
-    print(f"Superuser {username} created with password {password}")
+    print(f"Superuser {username!r} created with password {password!r}")
     input("Press enter to close.")
     return None
 
